5f2050bcab9cc086e07252afa82564b9b67652ce
Daniel Lugo <danlugo92@gmail.com>
2021-02-28 13:43:32 -0400

    Pad component

---
+++ src/common/Pad/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export interface PadProps {
+  amt: number;
+  insideRow?: boolean;
+}
+
+const Pad: React.FC<PadProps> = ({ amt, insideRow }) => {
+  const style: React.CSSProperties = {};
+  if (insideRow) {
+    style.width = amt;
+  } else {
+    style.height = amt;
+  }
+
+  return <div style={style} />;
+};
+
+export default Pad;
