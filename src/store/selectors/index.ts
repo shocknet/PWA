561389c73ebac2420f72cc3ab981ba176a36e059
Daniel Lugo <danlugo92@gmail.com>
2021-02-28 15:27:28 -0400

    custom useSelector hook

--- src/store/selectors/index.ts
+++ src/store/selectors/index.ts
@@ -1,2 +1,12 @@
+import {
+  useSelector as originalUseSelector,
+  TypedUseSelectorHook
+} from "react-redux";
+
+import { State } from "../../reducers";
+
+export const useSelector: TypedUseSelectorHook<State> = (selector, eqFn) =>
+  originalUseSelector(selector, eqFn);
+
 export * from "./auth";
 export * from "./users";
