f6170808d26bd88b36b02e7f7455134f5b7b7837
Daniel Lugo <danlugo92@gmail.com>
2021-03-24 17:34:05 -0400

    place ShockAvatar in overview screen

--- src/common/MainNav/index.js
+++ src/common/MainNav/index.js
@@ -2,9 +2,10 @@
 import React, { useCallback } from "react";
 import classNames from "classnames";
 import "./css/index.css";
-import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { openDrawer } from "../../actions/DrawerActions";
+import ShockAvatar from "../ShockAvatar";
+import { useSelector } from "../../store";
 
 const MainNav = ({
   pageTitle,
@@ -22,6 +23,8 @@ const MainNav = ({
     dispatch(openDrawer());
   }, [dispatch]);
 
+  const gunPublicKey = useSelector(({ node }) => node.publicKey);
+
   return (
     <div
       className={classNames({
@@ -35,13 +38,7 @@ const MainNav = ({
           <i className="icon icon-thin-back"></i>
         </div>
       ) : (
-        <Link
-          to="/"
-          className="main-nav-avatar"
-          style={{
-            backgroundImage: avatar ? `url(${avatar})` : null
-          }}
-        />
+        <ShockAvatar disableOnlineRing height={40} publicKey={gunPublicKey} />
       )}
       <p className="main-nav-title unselectable">{pageTitle}</p>
       <div className="main-nav-menu-btn" onClick={open}>
