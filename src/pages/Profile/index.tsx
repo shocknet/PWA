7dbe2994c4129b9423f05b68e97f8ed28417f350
Daniel Lugo <danlugo92@gmail.com>
2021-03-29 16:52:24 -0400

    wrap in fragment

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -229,232 +229,237 @@ const ProfilePage = () => {
       });
   };
   return (
-    <div className="page-container profile-page">
-      <div className="profile-container">
-        <div className="profile-cover" />
-        <div className="profile-info-container">
-          <div
-            className="profile-avatar"
-            style={{
-              height: `${AVATAR_SIZE}px`,
-              width: `${AVATAR_SIZE}px`
-            }}
-          >
-            <ShockAvatar
-              height={AVATAR_SIZE}
-              publicKey={publicKey}
-              setsAvatar
-            />
-          </div>
-
-          <div className="profile-info">
-            <p className="profile-name" onClick={() => {}}>
-              {processedDisplayName}
-            </p>
-            <p className="profile-desc">{user.bio || "Shockwallet user"}</p>
-            <div className="config-btn" onClick={toggleConfigModal}>
-              <i className="config-btn-icon icon-solid-spending-rule" />
-              <p className="config-btn-text">Config</p>
-            </div>
-          </div>
-        </div>
-        <div>
-          <Link to={"/goLive"} className="profile-choice-container">
+    <>
+      <div className="page-container profile-page">
+        <div className="profile-container">
+          <div className="profile-cover" />
+          <div className="profile-info-container">
             <div
+              className="profile-avatar"
               style={{
-                backgroundColor: "red",
-                color: "white",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                padding: "0.2em 0.5em",
-                borderRadius: "0.7em",
-                fontSize: "16px",
-                fontWeight: 600
+                height: `${AVATAR_SIZE}px`,
+                width: `${AVATAR_SIZE}px`
               }}
             >
-              <i className="fas fa-video"></i>
-              <p>GO LIVE</p>
+              <ShockAvatar
+                height={AVATAR_SIZE}
+                publicKey={publicKey}
+                setsAvatar
+              />
             </div>
-          </Link>
-          <Link to={"/createPost"} className="profile-choice-container">
-            <i className="profile-choice-icon fas fa-pen-square"></i>
-            <p className="profile-choice-text">Create Post</p>
-          </Link>
-          <Link to={"/publishContent"} className="profile-choice-container">
-            <i className="profile-choice-icon fab fa-youtube"></i>
-            <p className="profile-choice-text">Publish Content</p>
-          </Link>
-          <button className="profile-choice-container">
-            <i className="profile-choice-icon fas fa-shopping-cart"></i>
-            <p className="profile-choice-text">Offer a Product</p>
-          </button>
-          <Link to={"/offerService"} className="profile-choice-container">
-            <i className="profile-choice-icon fas fa-running"></i>
-            <p className="profile-choice-text">Offer a Service</p>
-          </Link>
-        </div>
-        <div className="">
-          <select
-            value={selectedView}
-            name="selectedView"
-            onChange={onInputChange}
-          >
-            <option value="posts">POSTS</option>
-            <option value="services">SERVICES</option>
-          </select>
-          {selectedView === "posts" && renderPosts()}
-          {selectedView === "services" && renderServices()}
-          {myPosts.map((post, index) => {
-            const profile = userProfiles[post.authorId];
-            if (post.type === "shared") {
-              const originalPublicKey = post.originalAuthor;
-              const originalProfile = userProfiles[originalPublicKey];
+
+            <div className="profile-info">
+              <p className="profile-name" onClick={() => {}}>
+                {processedDisplayName}
+              </p>
+              <p className="profile-desc">{user.bio || "Shockwallet user"}</p>
+              <div className="config-btn" onClick={toggleConfigModal}>
+                <i className="config-btn-icon icon-solid-spending-rule" />
+                <p className="config-btn-text">Config</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <Link to={"/goLive"} className="profile-choice-container">
+              <div
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: "0.2em 0.5em",
+                  borderRadius: "0.7em",
+                  fontSize: "16px",
+                  fontWeight: 600
+                }}
+              >
+                <i className="fas fa-video"></i>
+                <p>GO LIVE</p>
+              </div>
+            </Link>
+            <Link to={"/createPost"} className="profile-choice-container">
+              <i className="profile-choice-icon fas fa-pen-square"></i>
+              <p className="profile-choice-text">Create Post</p>
+            </Link>
+            <Link to={"/publishContent"} className="profile-choice-container">
+              <i className="profile-choice-icon fab fa-youtube"></i>
+              <p className="profile-choice-text">Publish Content</p>
+            </Link>
+            <button className="profile-choice-container">
+              <i className="profile-choice-icon fas fa-shopping-cart"></i>
+              <p className="profile-choice-text">Offer a Product</p>
+            </button>
+            <Link to={"/offerService"} className="profile-choice-container">
+              <i className="profile-choice-icon fas fa-running"></i>
+              <p className="profile-choice-text">Offer a Service</p>
+            </Link>
+          </div>
+          <div className="">
+            <select
+              value={selectedView}
+              name="selectedView"
+              onChange={onInputChange}
+            >
+              <option value="posts">POSTS</option>
+              <option value="services">SERVICES</option>
+            </select>
+            {selectedView === "posts" && renderPosts()}
+            {selectedView === "services" && renderServices()}
+            {myPosts.map((post, index) => {
+              const profile = userProfiles[post.authorId];
+              if (post.type === "shared") {
+                const originalPublicKey = post.originalAuthor;
+                const originalProfile = userProfiles[originalPublicKey];
+                return (
+                  <Suspense fallback={<Loader />} key={index}>
+                    <SharedPost
+                      originalPost={post.originalPost}
+                      originalPostProfile={originalProfile}
+                      sharedTimestamp={post.shareDate}
+                      sharerProfile={profile}
+                      postPublicKey={originalPublicKey}
+                      openTipModal={() => {}}
+                      // TODO: User online status handling
+                      isOnlineNode
+                      openUnlockModal={false}
+                    />
+                  </Suspense>
+                );
+              }
+
               return (
                 <Suspense fallback={<Loader />} key={index}>
-                  <SharedPost
-                    originalPost={post.originalPost}
-                    originalPostProfile={originalProfile}
-                    sharedTimestamp={post.shareDate}
-                    sharerProfile={profile}
-                    postPublicKey={originalPublicKey}
+                  <Post
+                    id={post.id}
+                    timestamp={post.date}
+                    contentItems={post.contentItems}
+                    avatar={`data:image/png;base64,${profile?.avatar}`}
+                    username={processDisplayName(
+                      profile?.publicKey,
+                      profile?.displayName
+                    )}
+                    publicKey={post.authorId}
                     openTipModal={() => {}}
                     // TODO: User online status handling
                     isOnlineNode
                     openUnlockModal={false}
+                    tipCounter={undefined}
+                    tipValue={undefined}
                   />
                 </Suspense>
               );
-            }
-
-            return (
-              <Suspense fallback={<Loader />} key={index}>
-                <Post
-                  id={post.id}
-                  timestamp={post.date}
-                  contentItems={post.contentItems}
-                  avatar={`data:image/png;base64,${profile?.avatar}`}
-                  username={processDisplayName(
-                    profile?.publicKey,
-                    profile?.displayName
-                  )}
-                  publicKey={post.authorId}
-                  openTipModal={() => {}}
-                  // TODO: User online status handling
-                  isOnlineNode
-                  openUnlockModal={false}
-                  tipCounter={undefined}
-                  tipValue={undefined}
-                />
-              </Suspense>
-            );
-          })}
-        </div>
-        <Modal
-          toggleModal={toggleModal}
-          modalOpen={profileModalOpen}
-          contentStyle={{
-            padding: "40px 30px"
-          }}
-        >
-          <QRCode
-            bgColor="#23282d"
-            fgColor="#4285b9"
-            value={publicKey}
-            size={180}
-            className="profile-qrcode"
-          />
-          <p className="profile-qrcode-desc">
-            Other users can scan this code to contact you
-          </p>
-          <div className="profile-clipboard-container" onClick={copyClipboard}>
-            <img
-              src={ClipboardIcon}
-              className="profile-clipboard-icon"
-              alt=""
-            />
-            <p className="profile-clipboard-text">Tap to copy to clipboard</p>
+            })}
           </div>
-        </Modal>
+          <Modal
+            toggleModal={toggleModal}
+            modalOpen={profileModalOpen}
+            contentStyle={{
+              padding: "40px 30px"
+            }}
+          >
+            <QRCode
+              bgColor="#23282d"
+              fgColor="#4285b9"
+              value={publicKey}
+              size={180}
+              className="profile-qrcode"
+            />
+            <p className="profile-qrcode-desc">
+              Other users can scan this code to contact you
+            </p>
+            <div
+              className="profile-clipboard-container"
+              onClick={copyClipboard}
+            >
+              <img
+                src={ClipboardIcon}
+                className="profile-clipboard-icon"
+                alt=""
+              />
+              <p className="profile-clipboard-text">Tap to copy to clipboard</p>
+            </div>
+          </Modal>
 
-        <Modal
-          toggleModal={toggleConfigModal}
-          modalOpen={profileConfigModalOpen}
-          contentStyle={{
-            padding: "2em 2em",
-            height: "100%"
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
+          <Modal
+            toggleModal={toggleConfigModal}
+            modalOpen={profileConfigModalOpen}
+            contentStyle={{
+              padding: "2em 2em",
               height: "100%"
             }}
           >
-            <label htmlFor="newDisplayName">Display Name</label>
-            <input
-              type="text"
-              className="input-field"
-              placeholder={user.displayName || "new display name"}
-              name="newDisplayName"
-              onChange={({ target: { value } }) => {
-                setNewDisplayName(value);
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                height: "100%"
               }}
-            />
+            >
+              <label htmlFor="newDisplayName">Display Name</label>
+              <input
+                type="text"
+                className="input-field"
+                placeholder={user.displayName || "new display name"}
+                name="newDisplayName"
+                onChange={({ target: { value } }) => {
+                  setNewDisplayName(value);
+                }}
+              />
 
-            <label htmlFor="newBio">New Bio</label>
-            <input
-              type="text"
-              className="input-field"
-              placeholder={user.displayName || "new bio"}
-              name="newBio"
-              onChange={({ target: { value } }) => {
-                setNewBio(value);
-              }}
-            />
+              <label htmlFor="newBio">New Bio</label>
+              <input
+                type="text"
+                className="input-field"
+                placeholder={user.displayName || "new bio"}
+                name="newBio"
+                onChange={({ target: { value } }) => {
+                  setNewBio(value);
+                }}
+              />
 
-            <label htmlFor="localPub">Seed Service Provider</label>
-            <input
-              type="text"
-              className="input-field"
-              placeholder={localSeedPub}
-              name="localPub"
-              onChange={onInputChange}
-            />
-            {somethingInsideConfigModalChanged && (
-              <div className="flex-center" style={{ marginTop: "auto" }}>
-                <button
-                  onClick={onConfigCancel}
-                  className="shock-form-button m-1"
-                >
-                  CANCEL
-                </button>
-                <button
-                  onClick={onConfigSubmit}
-                  className="shock-form-button-confirm m-1"
-                >
-                  SUBMIT
-                </button>
-              </div>
-            )}
-          </div>
-        </Modal>
+              <label htmlFor="localPub">Seed Service Provider</label>
+              <input
+                type="text"
+                className="input-field"
+                placeholder={localSeedPub}
+                name="localPub"
+                onChange={onInputChange}
+              />
+              {somethingInsideConfigModalChanged && (
+                <div className="flex-center" style={{ marginTop: "auto" }}>
+                  <button
+                    onClick={onConfigCancel}
+                    className="shock-form-button m-1"
+                  >
+                    CANCEL
+                  </button>
+                  <button
+                    onClick={onConfigSubmit}
+                    className="shock-form-button-confirm m-1"
+                  >
+                    SUBMIT
+                  </button>
+                </div>
+              )}
+            </div>
+          </Modal>
 
-        <AddBtn
-          onClick={toggleModal}
-          large
-          iconURL={QRCodeIcon}
-          style={{ backgroundColor: "var(--yellow)" }}
-          icon={null}
-          label={null}
-        />
+          <AddBtn
+            onClick={toggleModal}
+            large
+            iconURL={QRCodeIcon}
+            style={{ backgroundColor: "var(--yellow)" }}
+            icon={null}
+            label={null}
+          />
+        </div>
+
+        <BottomBar />
       </div>
-
-      <BottomBar />
-    </div>
+    </>
   );
 };
 
