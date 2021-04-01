a9a6154c53ac9116262d5a0c23264d011af5cc3e
Daniel Lugo <danlugo92@gmail.com>
2021-03-31 20:21:08 -0400

    util for normalization of timestamps

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -143,3 +143,32 @@ export const retryOperation = <T>(
         return reject(reason);
       });
   });
+
+/**
+ * Converts seconds/microseconds timestamps to milliseconds, leaves milliseconds
+ * timestamps untouched. Works for timestamps no older than 2001.
+ * @timestamp A timestamp that can be seconds, milliseconds or microseconds.
+ * Should be no older than 2001.
+ */
+export function normalizeTimestampToMs(timestamp: number): number {
+  if (timestamp === 0) {
+    return timestamp;
+  }
+
+  const t = timestamp.toString();
+
+  if (t.length === 10) {
+    // is seconds
+    return Number(t) * 1000;
+  } else if (t.length === 13) {
+    // is milliseconds
+    return Number(t);
+  } else if (t.length === 16) {
+    // is microseconds
+    return Number(t) / 1000;
+  }
+
+  logger.error("normalizeTimestamp() -> could not interpret timestamp");
+
+  return Number(t);
+}
