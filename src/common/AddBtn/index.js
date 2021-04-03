f4b80a84d411e4682ddc68315966a7d21693749a
Daniel Lugo <danlugo92@gmail.com>
2021-04-03 19:45:11 -0400

    remove unused react import

--- src/common/AddBtn/index.js
+++ src/common/AddBtn/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import classNames from "classnames";
 import "./css/index.css";
 
