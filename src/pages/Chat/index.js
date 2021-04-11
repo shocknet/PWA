4ff82ff0280ff828a3e340640abf9a8f84ea5427
Daniel Lugo <danlugo92@gmail.com>
2021-04-11 13:57:31 -0400

    Remove unused import

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import TextArea from "react-textarea-autosize";
