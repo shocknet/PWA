31b42abaa6125165685743af2470305b4be2f612
Daniel Lugo <danlugo92@gmail.com>
2021-04-11 14:34:43 -0400

    Change to non-arrow function

--- src/pages/Messages/components/Request/index.js
+++ src/pages/Messages/components/Request/index.js
@@ -2,20 +2,21 @@ import { Link } from "react-router-dom";
 import { processDisplayName } from "../../../../utils/String";
 import "./css/index.css";
 
-const Request = ({ publicKey = "", displayName = "", time, sent }) => (
-  <Link to={`/chat/${publicKey}`} className="request-container">
-    <div className="request-author-container">
-      <div className="request-author-avatar"></div>
-      <div className="request-author-details">
-        <p className="request-author-username">
-          {sent ? "Sent Request" : "Received Request"} -{" "}
-          {processDisplayName(publicKey, displayName)}
-        </p>
-        <p className="request-author-text">Request pending acceptance...</p>
+const Request = ({ publicKey = "", displayName = "", time, sent }) => {
+  return (
+    <Link to={`/chat/${publicKey}`} className="request-container">
+      <div className="request-author-container">
+        <div className="request-author-avatar"></div>
+        <div className="request-author-details">
+          <p className="request-author-username">
+            {sent ? "Sent Request" : "Received Request"} -{" "}
+            {processDisplayName(publicKey, displayName)}
+          </p>
+          <p className="request-author-text">Request pending acceptance...</p>
+        </div>
       </div>
-    </div>
-    <p className="request-timestamp">{time}</p>
-  </Link>
-);
+      <p className="request-timestamp">{time}</p>
+    </Link>
+  );
 
 export default Request;
