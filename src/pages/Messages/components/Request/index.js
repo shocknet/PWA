80225dccea232b855ec9ddc987c24d1222b54042
Daniel Lugo <danlugo92@gmail.com>
2021-04-12 12:39:17 -0400

    Use display name from users slice

--- src/pages/Messages/components/Request/index.js
+++ src/pages/Messages/components/Request/index.js
@@ -1,17 +1,20 @@
 // @ts-check
 import { Link } from "react-router-dom";
-import { processDisplayName } from "../../../../utils/String";
+
+import * as Store from "../../../../store";
+
 import "./css/index.css";
 
-const Request = ({ publicKey = "", displayName = "", time, sent }) => {
+const Request = ({ publicKey = "", time, sent }) => {
+  const user = Store.useSelector(Store.selectUser(publicKey));
+
   return (
     <Link to={`/chat/${publicKey}`} className="request-container">
       <div className="request-author-container">
         <div className="request-author-avatar"></div>
         <div className="request-author-details">
           <p className="request-author-username">
-            {sent ? "Sent Request" : "Received Request"} -{" "}
-            {processDisplayName(publicKey, displayName)}
+            {sent ? "Sent Request" : "Received Request"} - {user.displayName}
           </p>
           <p className="request-author-text">Request pending acceptance...</p>
         </div>
@@ -19,5 +22,6 @@ const Request = ({ publicKey = "", displayName = "", time, sent }) => {
       <p className="request-timestamp">{time}</p>
     </Link>
   );
+};
 
 export default Request;
