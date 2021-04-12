b4039a6ae882e3d8f25b0354c6dac8f31ef3558d
Daniel Lugo <danlugo92@gmail.com>
2021-04-12 15:46:54 -0400

    ChatMessage schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -21,3 +21,22 @@ export interface SentRequest {
   timestamp: number;
   loading: boolean;
 }
+
+export type ChatMessageStatus = "SENT" | "SENDING" | "FAILED" | "RECEIVED";
+
+export const CHAT_MESSAGE_STATUS = {
+  SENT: "SENT",
+  SENDING: "SENDING",
+  FAILED: "FAILED",
+  RECEIVED: "RECEIVED"
+} as const;
+
+export interface ChatMessage {
+  body: string;
+  id: string;
+  localId: string;
+  outgoing: boolean;
+  recipientPublicKey: string;
+  status: ChatMessageStatus;
+  timestamp: number;
+}
