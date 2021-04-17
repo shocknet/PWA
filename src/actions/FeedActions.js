1f86b5c8e1f86453b1adbbd523dab59d05d8a8cb
Daniel Lugo <danlugo92@gmail.com>
2021-04-17 19:31:51 -0400

    Chore: format

--- src/actions/FeedActions.js
+++ src/actions/FeedActions.js
@@ -208,11 +208,12 @@ export const sendTipPost = ({
   });
 };
 
-export const deleteUserPost = ({id,authorId}) => dispatch => {
+export const deleteUserPost = ({ id, authorId }) => dispatch => {
   dispatch({
-    type:ACTIONS.DELETE_USER_POST,
-    data:{
-      id,authorId
+    type: ACTIONS.DELETE_USER_POST,
+    data: {
+      id,
+      authorId
     }
-  })
-}
+  });
+};
