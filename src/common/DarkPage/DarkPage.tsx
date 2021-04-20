a234b28c1ca469b29c31ed2eebeb07f8ba3acdd9
Daniel Lugo <danlugo92@gmail.com>
2021-04-20 15:35:32 -0400

    <DarkPage /> now supports flex justify

--- src/common/DarkPage/DarkPage.tsx
+++ src/common/DarkPage/DarkPage.tsx
@@ -1,26 +1,37 @@
+import * as R from "react";
 import c from "classnames";
 
-import * as R from "react";
-
 import DialogNav, { DIALOG_NAV_MIN_HEIGHT_PX } from "../DialogNav";
-
 import Pad from "../Pad";
+import * as gStyles from "../../styles";
 
 import styles from "./css/DarkPage.module.css";
 
 export interface DarkPageProps {
+  justify?: "start" | "center";
   pageTitle?: string;
   scrolls?: boolean;
 }
 
-const DarkPage: R.FC<DarkPageProps> = ({ children, pageTitle, scrolls }) => {
+const DarkPage: R.FC<DarkPageProps> = ({
+  children,
+  justify,
+  pageTitle,
+  scrolls
+}) => {
+  if (scrolls && !!justify) {
+    throw new Error(`Can not both set "scrolls" and "justify" at <DarkPage />`);
+  }
   //#region controller
   const [headerHeight, setHeaderHeight] = R.useState(DIALOG_NAV_MIN_HEIGHT_PX);
   //#endregion controller
 
   return (
     <div
-      className={c(styles.container, scrolls && styles["container-scrolls"])}
+      className={c(styles.container, scrolls && styles["container-scrolls"], {
+        [gStyles.col]: !!justify,
+        [gStyles.centerJustify]: justify === "center"
+      })}
     >
       <DialogNav pageTitle={pageTitle || ""} onHeight={setHeaderHeight} />
 
