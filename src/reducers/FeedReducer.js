fd763d53ff1dc67fda1135ed16d08e173aa91582
Daniel Lugo <danlugo92@gmail.com>
2021-04-22 11:28:59 -0400

    Chore: format (apply prettier)

--- src/reducers/FeedReducer.js
+++ src/reducers/FeedReducer.js
@@ -12,8 +12,9 @@ import { ACTIONS } from "../actions/FeedActions";
 const INITIAL_STATE = {
   follows: /** @type {Follow[]} */ ([
     {
-      user: "qsgziGQS99sPUxV1CRwwRckn9cG6cJ3prbDsrbL7qko.oRbCaVKwJFQURWrS1pFhkfAzrkEvkQgBRIUz9uoWtrg",
-      status: 'ok',
+      user:
+        "qsgziGQS99sPUxV1CRwwRckn9cG6cJ3prbDsrbL7qko.oRbCaVKwJFQURWrS1pFhkfAzrkEvkQgBRIUz9uoWtrg",
+      status: "ok",
       private: false
     }
   ]),
