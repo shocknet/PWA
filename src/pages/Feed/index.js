91915e4096e17d8e7d521e8fc0289db86364e86e
Daniel Lugo <danlugo92@gmail.com>
2021-04-25 10:16:56 -0400

    Chore: apply format (run prettier)

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -27,20 +27,20 @@ const SharedPost = React.lazy(() => import("../../common/Post/SharedPost"));
 
 const FeedPage = () => {
   const follows = Store.useSelector(Store.selectFollows);
-  useEffect(()=>{
-    console.info("follows in feed just updated:")
-    console.info(follows)
-  },[follows])
+  useEffect(() => {
+    console.info("follows in feed just updated:");
+    console.info(follows);
+  }, [follows]);
   const posts = Store.useSelector(({ feed }) => feed.posts);
-  useEffect(()=>{
-    console.info("posts in feed just updated:")
-    console.info(posts)
-  },[posts])
+  useEffect(() => {
+    console.info("posts in feed just updated:");
+    console.info(posts);
+  }, [posts]);
   const userProfiles = Store.useSelector(({ userProfiles }) => userProfiles);
-  useEffect(()=>{
-    console.info("userProfiles in feed just updated:")
-    console.info(userProfiles)
-  },[userProfiles])
+  useEffect(() => {
+    console.info("userProfiles in feed just updated:");
+    console.info(userProfiles);
+  }, [userProfiles]);
   const [tipModalData, setTipModalOpen] = useState(null);
   const [unlockModalData, setUnlockModalOpen] = useState(null);
   const { avatar } = Store.useSelector(Store.selectSelfUser);
@@ -112,10 +112,10 @@ const FeedPage = () => {
           {follows?.map(follow => {
             const publicKey = follow.user;
             const profile =
-            userProfiles[publicKey] ?? Common.createEmptyUser(publicKey);
-            
-            console.log(publicKey)
-            console.log(profile.displayName)
+              userProfiles[publicKey] ?? Common.createEmptyUser(publicKey);
+
+            console.log(publicKey);
+            console.log(profile.displayName);
             return (
               <UserIcon
                 username={processDisplayName(publicKey, profile.displayName)}
