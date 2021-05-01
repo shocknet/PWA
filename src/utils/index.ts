581978164c6ed16b70ee02123f890da84234c4b2
Daniel Lugo <danlugo92@gmail.com>
2021-05-01 12:55:14 -0400

    Debug level for common logger

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,7 +10,8 @@ export * from "./Error";
 export const logger = {
   log: (...args: any[]) => console.log(...args),
   warn: (...args: any[]) => console.warn(...args),
-  error: (...args: any[]) => console.error(...args)
+  error: (...args: any[]) => console.error(...args),
+  debug: (...args: any[]) => console.debug(...args)
 };
 
 export interface File {
