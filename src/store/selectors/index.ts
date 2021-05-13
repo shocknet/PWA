b86b82c8683e2bb2515a45778349b37a5f745540
Daniel Lugo <danlugo92@gmail.com>
2021-05-13 11:09:45 -0400

    Root state selector

--- src/store/selectors/index.ts
+++ src/store/selectors/index.ts
@@ -8,6 +8,8 @@ import { State } from "../../reducers";
 export const useSelector: TypedUseSelectorHook<State> = (selector, eqFn) =>
   originalUseSelector(selector, eqFn);
 
+export const selectRoot = (state: State) => state;
+
 export * from "./auth";
 export * from "./users";
 export * from "./follows";
