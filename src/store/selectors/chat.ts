7ef159575f81bd02ecdccfca25f33b3d3ee26813
Daniel Lugo <danlugo92@gmail.com>
2021-05-19 14:47:16 -0400

    Memoize received requests selector

--- src/store/selectors/chat.ts
+++ src/store/selectors/chat.ts
@@ -1,3 +1,5 @@
+import { createSelector } from "reselect";
+
 import { State } from "../../reducers";
 import * as Schema from "../../schema";
 
@@ -18,14 +20,14 @@ export const selectIsInContact = (publicKey: string) => (
 export const selectCurrentHandshakeAddress = (state: State) =>
   state.chat.currentHandshakeAddress;
 
-export const selectReceivedRequests = (
-  state: State
-): Schema.ReceivedRequest[] => {
-  const { receivedRequests, recipientToOutgoing } = state.chat;
+export const selectReceivedRequests = createSelector(
+  (state: State) => state.chat.receivedRequests,
+  (state: State) => state.chat.recipientToOutgoing,
+  (receivedRequests, recipientToOutgoing): Schema.ReceivedRequest[] => {
+    return receivedRequests.filter(req => {
+      const conversationExists = recipientToOutgoing[req.pk];
 
-  return receivedRequests.filter(req => {
-    const conversationExists = recipientToOutgoing[req.pk];
-
-    return !conversationExists;
-  });
-};
+      return !conversationExists;
+    });
+  }
+);
