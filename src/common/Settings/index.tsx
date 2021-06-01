a568dfc9d98cb2efee32baa8ce0a647aa96fe41c
Daniel Lugo <danlugo92@gmail.com>
2021-05-31 20:16:32 -0400

    <Settings.SectionTitle />

--- src/common/Settings/index.tsx
+++ src/common/Settings/index.tsx
@@ -6,3 +6,7 @@ export * from "./SettingOrData";
 
 export { default as SmallData } from "./SmallData";
 export * from "./SmallData";
+
+export const SectionTitle: React.FC<{ children: string }> = ({ children }) => (
+  <h4 className="text-align-left">{children}</h4>
+);
