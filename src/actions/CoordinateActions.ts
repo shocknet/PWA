cf055b812372d1ad05212abc7f386276a5e2fdd8
Daniel Lugo <danlugo92@gmail.com>
2021-06-20 15:37:49 -0400

    Coordinates actions

---
+++ src/actions/CoordinateActions.ts
@@ -0,0 +1,61 @@
+import * as Common from "shock-common";
+import { createAction } from "@reduxjs/toolkit";
+
+import * as Utils from "../utils";
+
+export const coordinateDeleted = createAction<{
+  coordinateSHA256: string;
+}>("coordinates/deleted");
+
+export const coordinateReceived = createAction<{
+  coordinate: Common.Coordinate;
+  coordinateSHA256: string;
+}>("coordinates/received");
+
+export const subCoordinates = () => (dispatch: (action: any) => void) => {
+  Utils.logger.debug(`Subscribing to coordinates.`);
+  try {
+    const subscription = Utils.rifle({
+      query: `$user::coordinates::map.on`,
+      onData: (coordinate, coordinateSHA256) => {
+        Utils.logger.debug(
+          `Coordinate Subscription -> `,
+          coordinateSHA256,
+          coordinate
+        );
+
+        if (Common.isCoordinate(coordinate)) {
+          dispatch(
+            coordinateReceived({
+              coordinate,
+              coordinateSHA256
+            })
+          );
+        } else if (coordinate === null) {
+          dispatch(
+            coordinateDeleted({
+              coordinateSHA256
+            })
+          );
+        } else {
+          Utils.logger.error(
+            `Coordinate subscription invalid (or incomplete) value -> `,
+            coordinate
+          );
+        }
+      }
+    });
+
+    return () => {
+      subscription.then(sub => {
+        sub.off();
+      });
+    };
+  } catch (e) {
+    alert(`Could not establish a subscription to coordinates: ${e.message}`);
+    Utils.logger.error(
+      `Could not establish a subscription to coordinates -> `,
+      e
+    );
+  }
+};
