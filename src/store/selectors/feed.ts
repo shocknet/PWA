d3c5c925e6f7d18a0e596439e5b8b903c788ea4f
Daniel Lugo <danlugo92@gmail.com>
2021-06-30 09:13:20 -0400

    Posts selectors

--- src/store/selectors/feed.ts
+++ src/store/selectors/feed.ts
@@ -27,3 +27,24 @@ export const selectSingleSelfPost = (postID: string) => (
   const selfPublicKey = selectSelfPublicKey(state);
   return selectSinglePost(selfPublicKey, postID)(state);
 };
+
+export const selectPosts = (author: string) => (
+  state: State
+): Array<Schema.Post | Schema.SharedPost> => {
+  return state.feed.posts[author] || [];
+};
+
+export const selectPostsNewestToOldest = (author: string) => (
+  state: State
+): Array<Schema.Post | Schema.SharedPost> => {
+  const posts = state.feed.posts[author]?.slice() || [];
+
+  posts.sort((a, b) => {
+    const alpha = Schema.isSharedPost(a) ? a.shareDate : a.date;
+    const beta = Schema.isSharedPost(b) ? b.shareDate : b.date;
+
+    return beta - alpha;
+  });
+
+  return posts;
+};
