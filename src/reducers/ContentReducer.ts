d670d02ba467cab2710c6b6968dcbb9c4904fc3f
CapDog <34176400+capitalistdog@users.noreply.github.com>
2021-07-03 17:00:14 -0400

    Lightning Page default provider

--- src/reducers/ContentReducer.ts
+++ src/reducers/ContentReducer.ts
@@ -10,7 +10,7 @@ import { PublishedContent, PublicContentItem } from "../schema";
 
 const INITIAL_STATE = {
   seedProviderPub:
-    "JVz7h3yUnbgMwwKxSddGenBlrE9eeDJVYWlmOr941mI.LW5PEWM3Y-DRf-UApdSN76wH6id6zR4mXNyBApihoAA",
+    "tcUUzRkyzXYhIZQbmopiCLREyZ_kQJqQ-C4XesecOm4.GX1Dv-eGcfKuOPobBK9Q-Sc-o697XgVCQzOCfqfimIo",
   streamUserToken: "",
   streamLiveToken: "",
   streamUrl: "",
