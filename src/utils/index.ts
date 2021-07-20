f5a86657684c69fd70e178f0c74fd6b4b088dbf8
Daniel Lugo <danlugo92@gmail.com>
2021-07-19 22:37:58 -0400

    safeParseJson util (lost in merge)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -209,6 +209,23 @@ export const useBooleanState = (initialState: boolean) => {
 
 export const parseJson = (o: string) => JSON.parse(o) as unknown;
 
+/**
+ * Returns null if the value provided is not a valid JSON string.
+ * @param o
+ */
+export const safeParseJson = (o: unknown): unknown => {
+  if (!Common.isPopulatedString(o)) {
+    return null;
+  }
+  try {
+    return JSON.parse(o);
+  } catch (e) {
+    logger.warn(`Error inside safeParseJson() -> `, e);
+    logger.log(`Error inside safeParseJson(), string provided -> `, o);
+    return null;
+  }
+};
+
 export const useDispatch = (): ThunkDispatch<State, undefined, Action> => {
   return originalUseDispatch() as ThunkDispatch<State, undefined, Action>;
 };
