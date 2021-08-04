49c3be8417b3473a6cd21784371fe5654dbb64ca
Daniel Lugo <danlugo92@gmail.com>
2021-08-04 15:22:52 -0400

    Optional blue btn in modal

--- src/common/Modal/index.js
+++ src/common/Modal/index.js
@@ -1,5 +1,8 @@
 import { useCallback } from "react";
 import classNames from "classnames";
+
+import * as Utils from "../../utils";
+
 import ModalContent from "./components/ModalContent";
 import ModalTitle from "./components/ModalTitle";
 import "./css/index.scoped.css";
@@ -13,7 +16,10 @@ const Modal = ({
   disableBackdropClose = false,
   forceRenderTitleBar = false,
   hideXBtn = false,
-  noFullWidth = false
+  noFullWidth = false,
+  blueBtn = "",
+  disableBlueBtn = false,
+  onClickBlueBtn = Utils.EMPTY_FN
 }) => {
   const closeModal = useCallback(() => {
     toggleModal();
@@ -39,6 +45,16 @@ const Modal = ({
           hideXBtn={hideXBtn}
         />
         <ModalContent style={contentStyle}>{children}</ModalContent>
+
+        {blueBtn && (
+          <button
+            disabled={disableBlueBtn}
+            className="submit-btn"
+            onClick={onClickBlueBtn}
+          >
+            {blueBtn}
+          </button>
+        )}
       </div>
     </div>
   );
