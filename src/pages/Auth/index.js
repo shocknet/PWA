46230c05c8207a6871f5d908fd9678b6a68be6e0
Daniel Lugo <danlugo92@gmail.com>
2021-08-04 09:39:29 -0400

    New default follow

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -47,8 +47,16 @@ const AuthPage = () => {
       dispatch(
         reloadFollows([
           {
+            // Lightning.Page
             user:
               "tcUUzRkyzXYhIZQbmopiCLREyZ_kQJqQ-C4XesecOm4.GX1Dv-eGcfKuOPobBK9Q-Sc-o697XgVCQzOCfqfimIo",
+            status: "ok",
+            private: false
+          },
+          {
+            // Guy Swang
+            user:
+              "CDfURSDaABun7sbzxt1LvUrTrtgLza_3NrCKrmUWIYU.Ssr3U9JN6cpFI9BZLL-1HLM2vS0ECEt-H4iUXvhkGzU",
             status: "ok",
             private: false
           }
