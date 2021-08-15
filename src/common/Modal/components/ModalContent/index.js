aa1a24d2aa182471482bfb0b918a62a41f1717d5
Daniel Lugo <danlugo92@gmail.com>
2021-08-15 15:25:27 -0400

    Naming

--- src/common/Modal/components/ModalContent/index.js
+++ src/common/Modal/components/ModalContent/index.js
@@ -1,9 +1,9 @@
-import c from "classnames";
+import classNames from "classnames";
 
 import "./css/index.scoped.css";
 
 const ModalContent = ({ children = null, style = {}, contentClass = "" }) => (
-  <div className={c("content", contentClass)} style={style}>
+  <div className={classNames("content", contentClass)} style={style}>
     {children}
   </div>
 );
