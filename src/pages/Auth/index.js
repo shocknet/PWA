f5316b5e9b573f3e715dde1696c12c63c1db0bf5
CapDog <34176400+capitalistdog@users.noreply.github.com>
2021-08-20 09:26:00 -0400

    Update index.js

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -54,9 +54,16 @@ const AuthPage = () => {
             private: false
           },
           {
-            // Guy Swang
+            // Guy Swann
             user:
               "CDfURSDaABun7sbzxt1LvUrTrtgLza_3NrCKrmUWIYU.Ssr3U9JN6cpFI9BZLL-1HLM2vS0ECEt-H4iUXvhkGzU",
+            status: "ok",
+            private: false
+          },
+          {
+            // JohnOnChain
+            user:
+              "Fd_7lUhwH7JoHbg6UKnGO9rYsZBOuR1KSbpZz8-Sbcw.fUu68TPuBWl83LBKgAbuiy0r1hMqnUevzyEgqPYu0B4",
             status: "ok",
             private: false
           }
