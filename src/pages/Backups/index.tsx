fb511be3b1061bff8e9a343f68fbe3b151d5792c
Daniel Lugo <danlugo92@gmail.com>
2021-09-06 16:38:44 -0400

    Format

--- src/pages/Backups/index.tsx
+++ src/pages/Backups/index.tsx
@@ -5,37 +5,37 @@ import Http from "../../utils/Http";
 import * as Store from "../../store";
 
 const Backups = () => {
-  const [seedBackup,setSeedBackup] = useState('')
-  const [channelsBackup,setChannelsBackup] = useState(null)
-  const [copied,setCopied] = useState(false)
-  const {identity_pubkey:lndPub} = Store.useSelector(
-    ({ wallet }) => wallet.lightningInfo)
-  const gunPub = Store.useSelector(
-    ({ node }) => node.publicKey)
-    //effect for seed backup
-    useEffect(()=>{
-      Http.get(`/api/gun/user/once/seedBackup`, {
-        headers: {
-          'public-key-for-decryption': gunPub,
-        },
-      }).then(({ data: { data: seedBackup } }) => {
-        setSeedBackup(seedBackup)
-      })
-    },[])
-    //effect for channels backup
-    useEffect(()=>{
-      Http.get(`/api/gun/user/once/channelsBackup`, {
-        headers: {
-          'public-key-for-decryption': gunPub,
-        },
-      }).then(({ data: { data: channelsBackup } }) => {
-        setChannelsBackup(channelsBackup)
-      })
-    },[])
-    const copyChannelsBackup = useCallback(() => {
-      navigator.clipboard.writeText(JSON.stringify(channelsBackup));
-      setCopied(true);
-    }, [channelsBackup]);
+  const [seedBackup, setSeedBackup] = useState("");
+  const [channelsBackup, setChannelsBackup] = useState(null);
+  const [copied, setCopied] = useState(false);
+  const { identity_pubkey: lndPub } = Store.useSelector(
+    ({ wallet }) => wallet.lightningInfo
+  );
+  const gunPub = Store.useSelector(({ node }) => node.publicKey);
+  //effect for seed backup
+  useEffect(() => {
+    Http.get(`/api/gun/user/once/seedBackup`, {
+      headers: {
+        "public-key-for-decryption": gunPub
+      }
+    }).then(({ data: { data: seedBackup } }) => {
+      setSeedBackup(seedBackup);
+    });
+  }, []);
+  //effect for channels backup
+  useEffect(() => {
+    Http.get(`/api/gun/user/once/channelsBackup`, {
+      headers: {
+        "public-key-for-decryption": gunPub
+      }
+    }).then(({ data: { data: channelsBackup } }) => {
+      setChannelsBackup(channelsBackup);
+    });
+  }, []);
+  const copyChannelsBackup = useCallback(() => {
+    navigator.clipboard.writeText(JSON.stringify(channelsBackup));
+    setCopied(true);
+  }, [channelsBackup]);
   return (
     <div className="moonPayContainer">
       <MainNav pageTitle="BACKUPS" enableBackButton={true} />
@@ -51,11 +51,17 @@ const Backups = () => {
         </div>
         <div className="innerContainer">
           <h2>Channels Backups</h2>
-          {channelsBackup && <div className="copyContainer">
-            <p>copy channel backups: </p>
-            {copied && <i className="fas fa-check" onClick={copyChannelsBackup}></i>}
-            {!copied && <i className="fas fa-copy" onClick={copyChannelsBackup}></i>}
-          </div>}
+          {channelsBackup && (
+            <div className="copyContainer">
+              <p>copy channel backups: </p>
+              {copied && (
+                <i className="fas fa-check" onClick={copyChannelsBackup}></i>
+              )}
+              {!copied && (
+                <i className="fas fa-copy" onClick={copyChannelsBackup}></i>
+              )}
+            </div>
+          )}
           {!channelsBackup && <h3>backup not available on this node</h3>}
         </div>
       </div>
