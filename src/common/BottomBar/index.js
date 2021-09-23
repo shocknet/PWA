984f85139658170fc229d19d33976abdd885be3d
emad-salah <emads14@gmail.com>
2021-09-23 08:20:34 +0100

    Remove BottomBar for unauthenticated users

--- src/common/BottomBar/index.js
+++ src/common/BottomBar/index.js
@@ -1,36 +1,45 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "../../store";
 
-const BottomBar = () => (
-  <div className="bottom-nav-container">
-    <NavLink
-      className="bottom-nav-btn"
-      to="/overview"
-      activeClassName="active-nav-btn"
-    >
-      <i className="bottom-nav-btn-icon icon-thin-wallet"></i>
-    </NavLink>
-    <NavLink
-      className="bottom-nav-btn"
-      to="/chat"
-      activeClassName="active-nav-btn"
-    >
-      <i className="bottom-nav-btn-icon icon-thin-chat"></i>
-    </NavLink>
-    <NavLink
-      className="bottom-nav-btn"
-      to="/profile"
-      activeClassName="active-nav-btn"
-    >
-      <i className="bottom-nav-btn-icon icon-thin-profile"></i>
-    </NavLink>
-    <NavLink
-      className="bottom-nav-btn"
-      to="/feed"
-      activeClassName="active-nav-btn"
-    >
-      <i className="bottom-nav-btn-icon icon-thin-feed"></i>
-    </NavLink>
-  </div>
-);
+const BottomBar = () => {
+  const authenticated = useSelector(({ auth }) => auth.authenticated);
+
+  if (!authenticated) {
+    return null;
+  }
+
+  return (
+    <div className="bottom-nav-container">
+      <NavLink
+        className="bottom-nav-btn"
+        to="/overview"
+        activeClassName="active-nav-btn"
+      >
+        <i className="bottom-nav-btn-icon icon-thin-wallet"></i>
+      </NavLink>
+      <NavLink
+        className="bottom-nav-btn"
+        to="/chat"
+        activeClassName="active-nav-btn"
+      >
+        <i className="bottom-nav-btn-icon icon-thin-chat"></i>
+      </NavLink>
+      <NavLink
+        className="bottom-nav-btn"
+        to="/profile"
+        activeClassName="active-nav-btn"
+      >
+        <i className="bottom-nav-btn-icon icon-thin-profile"></i>
+      </NavLink>
+      <NavLink
+        className="bottom-nav-btn"
+        to="/feed"
+        activeClassName="active-nav-btn"
+      >
+        <i className="bottom-nav-btn-icon icon-thin-feed"></i>
+      </NavLink>
+    </div>
+  );
+};
 
 export default BottomBar;
