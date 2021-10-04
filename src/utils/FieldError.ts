8bedbd8538f9469a18f250ea27321e14de58cde7
emad-salah <emads14@gmail.com>
2021-10-04 08:42:09 +0100

    Improve FieldError constructor

--- src/utils/FieldError.ts
+++ src/utils/FieldError.ts
@@ -1,17 +1,20 @@
 export type FieldErrorType = {
   message: string;
   field: string;
+  data?: any;
   name?: string;
   stack?: string;
 };
 
 class FieldError extends Error {
   field: string;
+  data: any;
 
   constructor(error: any) {
     super();
     this.message = error?.message ?? "An unknown error has occurred";
     this.field = error?.field ?? "unknown";
+    this.data = error?.data;
     this.name = error?.name;
     this.stack = error?.stack;
   }
